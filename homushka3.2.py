399f3ad3f9a130f76f05c19d204eab2e2dc6277f
Maximus <Kharatin85@gmail.com>
2024-05-30 15:10:10 +0300

    Add HW 3.2

---
+++ homushka3.2.py
@@ -0,0 +1,38 @@
+lst = [12, 3, 4, 10]
+print(lst)
+if lst == []:
+    print(lst)
+else:
+    x = lst.pop(-1)
+    lst.insert(0, x)
+    print(lst)
+
+print('--------------')
+
+lst = [1]
+print(lst)
+if lst == []:
+    print(lst)
+else:
+    x = lst.pop(-1)
+    lst.insert(0, x)
+    print(lst)
+print('--------------')
+lst = []
+print(lst)
+if lst == []:
+    print(lst)
+else:
+    x = lst.pop(-1)
+    lst.insert(0, x)
+    print(lst)
+
+print('-----------------')
+lst = [12, 3, 4, 10, 8]
+print(lst)
+if lst == []:
+    print(lst)
+else:
+    x = lst.pop(-1)
+    lst.insert(0, x)
+    print(lst)
