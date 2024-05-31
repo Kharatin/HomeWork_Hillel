4095d08f1cefbcbb751e5b82caffeec79c290e2b
Maximus <Kharatin85@gmail.com>
2024-05-31 14:16:49 +0300

    Add HW 3.3

---
+++ homushka3.3.py
@@ -0,0 +1,39 @@
+my_list = [1, 2, 3, 4, 5, 6]
+x = len(my_list)
+y = ((x + 1 ) // 2)
+my_list1 = my_list[0:y]
+my_list2 = my_list[y:]
+my_new_list = [my_list1, my_list2]
+print(my_new_list)
+
+my_list = [1, 2, 3]
+x = len(my_list)
+y = ((x + 1 ) // 2)
+my_list1 = my_list[0:y]
+my_list2 = my_list[y:]
+my_new_list = [my_list1, my_list2]
+print(my_new_list)
+
+my_list = [1, 2, 3, 4, 5]
+x = len(my_list)
+y = ((x + 1 ) // 2)
+my_list1 = my_list[0:y]
+my_list2 = my_list[y:]
+my_new_list = [my_list1, my_list2]
+print(my_new_list)
+
+my_list = [1]
+x = len(my_list)
+y = ((x + 1 ) // 2)
+my_list1 = my_list[0:y]
+my_list2 = my_list[y:]
+my_new_list = [my_list1, my_list2]
+print(my_new_list)
+
+my_list = []
+x = len(my_list)
+y = ((x + 1 ) // 2)
+my_list1 = my_list[0:y]
+my_list2 = my_list[y:]
+my_new_list = [my_list1, my_list2]
+print(my_new_list)
