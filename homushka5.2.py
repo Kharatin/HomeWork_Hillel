3ab3ecaca4174858cd2a0c640c998168f4b9fea3
Maximus <Kharatin85@gmail.com>
2024-06-06 20:07:12 +0300

    Add HW  5.2

--- homushka5.2.py
+++ homushka5.2.py
@@ -8,26 +8,26 @@ while answer == 'yes' or answer == 'y':
     if action == '+':
         summa = number_one + number_two
         print('RESULT:', summa)
-        answer = input("Continue 'yes(y)' or ... ")
 
     elif action == '-':
         subtraction = number_one - number_two
         print('RESULT:', subtraction)
-        answer = input("Continue 'yes(y)' or ... ")
+
     elif action == '*':
         multiplication = number_one * number_two
         print('RESULT:', multiplication)
-        answer = input("Continue 'yes(y)' or ... ")
+
     elif action == '/':
         if number_two == 0:
             print("WRONG!!!!")
             print("YOU CAN'T DIVIDE BY ZERO !!!")
+
         else:
             division = number_one / number_two
             print('RESULT:', '%.1f' % (division))
-            answer = input("Continue 'yes(y)' or ... ")
+
     else:
         print("Something goes wrong.")
-        answer = input("Continue 'yes(y)' or ... ")
+    answer = input("Continue 'yes(y)' or ... ")
 print('\nBye Bye!')
 print("\t\tSee You Soon!")
