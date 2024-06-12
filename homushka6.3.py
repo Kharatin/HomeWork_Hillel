843e472c04b86bb0be7427fd5b760b164fb8b700
Maximus <Kharatin85@gmail.com>
2024-06-12 19:09:09 +0300

    Add HW  6.3

---
+++ homushka6.3.py
@@ -0,0 +1,37 @@
+num = str(input("Enter number: "))
+x = 1
+
+for i in num:
+    x *= int(i)
+num = str(x)
+
+x = 1
+for i in num:
+    x *= int(i)
+num = str(x)
+
+x = 1
+for i in num:
+    x *= int(i)
+num = str(x)
+
+x = 1
+for i in num:
+    x *= int(i)
+num = str(x)
+
+x = 1
+for i in num:
+    x *= int(i)
+num = str(x)
+
+x = 1
+for i in num:
+    x *= int(i)
+num = str(x)
+
+x = 1
+for i in num:
+    x *= int(i)
+
+print(num)
