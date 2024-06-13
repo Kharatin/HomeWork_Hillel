b1cc2923e07e29530c318db1ce4aa7b120685a13
Maximus <Kharatin85@gmail.com>
2024-06-13 18:22:51 +0300

    Add H.W 7.1

---
+++ homushka7.1.py
@@ -0,0 +1,13 @@
+
+
+def say_hi(name, age):
+    return f"Hi. My name is {name} and I'm {age} years old"
+
+
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
+
+
+mad = say_hi("Natasha", 18)
+print(mad)
