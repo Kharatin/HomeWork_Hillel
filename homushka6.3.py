8ffad23a4e7e32680b2f89afcd5ea85a94a9e1b0
Maximus <Kharatin85@gmail.com>
2024-06-13 16:39:25 +0300

    Add H.W 6.3 ;)

--- homushka6.3.py
+++ homushka6.3.py
@@ -33,5 +33,20 @@ num = str(x)
 x = 1
 for i in num:
     x *= int(i)
+num = str(x)
+
+x = 1
+for i in num:
+    x *= int(i)
+num = str(x)
+
+x = 1
+for i in num:
+    x *= int(i)
+num = str(x)
+
+x = 1
+for i in num:
+    x *= int(i)
 
 print(num)
