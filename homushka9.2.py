01602ec3ac0e39f16e552ddd4e357a1272b1940e
Maximus <Kharatin85@gmail.com>
2024-06-22 16:35:30 +0300

    Add H.W  9.2

--- homushka9.2.py
+++ homushka9.2.py
@@ -10,7 +10,6 @@ def difference(*arcs):
         answer = (lst[-1] - lst[0])
         return round(answer, 2)
 
-
 assert difference(1, 2, 3) == 2, 'Test1'
 assert difference(5, -5) == 10, 'Test2'
 assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
@@ -27,11 +26,8 @@ def difference(*arcs):
         d = max(arcs) - min(arcs)
         return round(d, 2)
 
-
 assert difference(1, 2, 3) == 2, 'Test1'
 assert difference(5, -5) == 10, 'Test2'
 assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
 assert difference() == 0, 'Test4'
 print('OK')
-
-
