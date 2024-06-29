eae13c55c483a4d9dc1b93479cacddba95ab3c1f
Maximus <Kharatin85@gmail.com>
2024-06-29 14:23:58 +0300

    Add H.W  11.3

---
+++ homushka11.3.py
@@ -0,0 +1,12 @@
+def is_even(number):
+    x = bin(number)
+    y = int(x[-1])
+    return y & 1 == 0
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
+print('OK')
+
+
+
+
